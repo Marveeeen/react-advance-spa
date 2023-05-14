2f0ae3e762b4b78d1853c208baf1b93381bf23b6
Marveeen <jmvroqueee@gmail.com>
2023-05-14 10:07:19 +0800

    feature: implement fetching events to the backend

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,5 +1,39 @@
+import { useEffect, useState } from "react";
+
 function EventsPage() {
-  return <div>EventsPage</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [fetchedEvents, setFetchedEvents] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchEvents() {
+      setIsLoading(true);
+      const response = await fetch("http://localhost:8080/events");
+
+      if (!response.ok) {
+        setError("Fetching events failed");
+      } else {
+        const resData = await response.json();
+        setFetchedEvents(resData);
+      }
+
+      setIsLoading(false);
+    }
+
+    fetchEvents();
+  }, []);
+
+  console.log(fetchedEvents);
+
+  return (
+    <>
+      <div style={{ textAlign: "center" }}>
+        {isLoading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+      </div>
+      {!isLoading && fetchedEvents && <p>Success</p>}
+    </>
+  );
 }
 
 export default EventsPage;

b2161263bfecf92126aafa29d2235cd6d034f2dd
Marveeen <jmvroqueee@gmail.com>
2023-05-20 11:26:59 +0800

    feature(EventDetail): add action that will allow user to delete an event

--- src/components/EventItem.jsx
+++ src/components/EventItem.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { Link, useSubmit } from "react-router-dom";
 
 import classes from "./EventItem.module.css";
-import { Link } from "react-router-dom";
 
 function EventItem({ event }) {
+  const submit = useSubmit();
+  function startDeleteHandler() {
+    const proceed = window.confirm("Are you sure?");
+
+    if (proceed) {
+      submit(null, { method: "delete" });
+    }
+  }
+
   return (
     <article className={classes.event}>
       <img src={event.image} alt={event.title} />
@@ -12,7 +21,7 @@ function EventItem({ event }) {
       <p>{event.description}</p>
       <menu className={classes.actions}>
         <Link to="edit">Edit</Link>
-        <button>Delete</button>
+        <button onClick={startDeleteHandler}>Delete</button>
       </menu>
     </article>
   );
--- src/pages/EventDetail.jsx
+++ src/pages/EventDetail.jsx
@@ -1,8 +1,8 @@
-import { json, useLoaderData, useRouteLoaderData } from "react-router-dom";
+import { json, redirect, useRouteLoaderData } from "react-router-dom";
 import EventItem from "../components/EventItem";
 
 function EventDetailPage() {
-  const data = useRouteLoaderData('event-detail');
+  const data = useRouteLoaderData("event-detail");
   return <EventItem event={data.event} />;
 }
 
@@ -21,5 +21,23 @@ export async function loader({ request, params }) {
     );
   } else {
     return response;
+  }
+}
+
+export async function action({ request, params }) {
+  const eventId = params.eventId;
+  const response = await fetch(`http://localhost:8080/events/${eventId}`, {
+    method: request.method,
+  });
+
+  if (!response.ok) {
+    throw json(
+      { message: "Could not delete event" },
+      {
+        status: 500,
+      }
+    );
+  } else {
+    return redirect("/events");
   }
 }

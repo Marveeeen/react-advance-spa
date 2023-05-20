cffaa9cdd7b77a425994e364085a59ec187191e3
Marveeen <jmvroqueee@gmail.com>
2023-05-20 10:41:11 +0800

    feature: add event form component

---
+++ src/components/EventForm.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import classes from "./EventForm.module.css";
+
+function EventForm({ method, event }) {
+  return (
+    <form className={classes.form}>
+      <p>
+        <label htmlFor="title">Title</label>
+        <input
+          id="title"
+          type="text"
+          name="title"
+          required
+          defaultValue={event ? event.title : ""}
+        />
+      </p>
+      <p>
+        <label htmlFor="image">Image</label>
+        <input
+          id="image"
+          type="url"
+          name="image"
+          required
+          defaultValue={event ? event.image : ""}
+        />
+      </p>
+      <p>
+        <label htmlFor="date">Date</label>
+        <input
+          id="date"
+          type="date"
+          name="date"
+          required
+          defaultValue={event ? event.date : ""}
+        />
+      </p>
+      <p>
+        <label htmlFor="description">Description</label>
+        <textarea
+          id="description"
+          type="description"
+          rows="5"
+          required
+          defaultValue={event ? event.description : ""}
+        />
+      </p>
+      <div className={classes.actions}>
+        <button type="button">Cancel</button>
+        <button>Save</button>
+      </div>
+    </form>
+  );
+}
+
+export default EventForm;

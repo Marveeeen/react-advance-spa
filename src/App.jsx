2decac1533f001d159120a7054f78f3508ed5b7c
Marveeen <jmvroqueee@gmail.com>
2023-05-20 09:59:58 +0800

    feature(ErrorPage): add error handling

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import EventDetailPage from "./pages/EventDetail";
 import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import EventsLayout from "./pages/EventsLayout";
+import ErrorPage from "./pages/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       {

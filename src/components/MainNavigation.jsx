9f9a6e486673922d6b9158429d1c4be063eca9ff
Marveeen <jmvroqueee@gmail.com>
2023-05-21 10:58:16 +0800

    refactor: remove newsletter navigation

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -28,7 +28,6 @@ function MainNavigation() {
           </li>
         </ul>
       </nav>
-      <NewsletterSignup />
     </header>
   );
 }
--- src/components/NewsletterSignup.jsx
+++ src/components/NewsletterSignup.jsx
@@ -1,15 +1,26 @@
+import { Form, useActionData } from "react-router-dom";
 import classes from "./NewsletterSignup.module.css";
+import { useEffect } from "react";
 
 function NewsletterSignup() {
+  const data = useActionData();
+
+  useEffect(() => {
+    if (data?.message) {
+      window.alert(data.message);
+    }
+  }, [data]);
+
   return (
-    <form action="post" className={classes.newsletter}>
+    <Form method="post" className={classes.newsletter}>
       <input
         type="email"
+        name="email"
         placeholder="Sign up for newsletter..."
         aria-label="Sign up for newsletter"
       />
       <button>Sign up</button>
-    </form>
+    </Form>
   );
 }
 

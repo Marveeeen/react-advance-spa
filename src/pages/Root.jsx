4842c8292a8fc4244d1c065b5dc59db7687f6027
Marveeen <jmvroqueee@gmail.com>
2023-05-26 19:01:27 +0800

    feature: add automatic logout when the token expires

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,7 +1,26 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useSubmit } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
+import { useEffect } from "react";
+import { getAuthToken, getTokenDuration } from "../utils/auth";
 
 function RootLayout() {
+  const token = getAuthToken();
+  const submit = useSubmit();
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === "EXPIRED") {
+      submit(null, { action: "/logout", method: "post" });
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+    setTimeout(() => {
+      submit(null, { action: "/logout", method: "post" });
+    }, tokenDuration);
+  }, [token, submit]);
   return (
     <>
       <MainNavigation />
